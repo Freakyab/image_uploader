6ffc79b99de29f4c39e8ff3204d2b64c89deb3e0
Freakyab <aryanbhisikar1@gmail.com>
2024-06-15 12:11:40 +0530

    Reverse the order of fetched images in ViewImage component

--- src/app/api/getImage/[slug]/route.ts
+++ src/app/api/getImage/[slug]/route.ts
@@ -2,17 +2,25 @@ import { NextRequest, NextResponse } from "next/server";
 import { getImageLink } from "@/app/actions/uploader";
 
 export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
-    try {
-        const id = params.slug.toString();
-        const image = await getImageLink(id);
-        if (image === null) {
-            return NextResponse.error();
-        } else {
-            const imageLink = image.link;
-            return NextResponse.json(imageLink);
-        }
-    } catch (err) {
-        console.log(err);
-        return NextResponse.error();
+  try {
+    const id = params.slug.toString();
+    const image = await getImageLink(id);
+
+    if (image === null) {
+      return new NextResponse("Image not found", { status: 404 });
+    } else {
+      const imageLink = image.link;
+      const response = NextResponse.json({ imageUrl: imageLink });
+
+      // Set CORS headers
+      response.headers.set("Access-Control-Allow-Origin", "*");
+      response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+      response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+      return response;
     }
+  } catch (err) {
+    console.log(err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
